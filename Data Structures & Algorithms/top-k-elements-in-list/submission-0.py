e62f1e38fc9a390a87d29babfec7b74e1b07d3f0
1Divy1 <103454669+1Divy1@users.noreply.github.com>
2026-04-04 12:17:59 +0300

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # USING A MAX-HEAP
+
+        max_heap = []
+        hash_map = {}
+
+        # create the frequency hash map
+        for n in nums:
+            if n not in hash_map:
+                hash_map[n] = 0
+            hash_map[n] += 1
+
+        for key, val in hash_map.items():
+            max_heap.append([-val, key])
+
+        heapq.heapify(max_heap)
+
+        output = []
+        while k > 0:
+            _, key = heapq.heappop(max_heap)
+            output.append(key)
+            k -= 1
+
+        return output
